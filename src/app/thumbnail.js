497bf44fc0049d93621229175d7cdb33fad2f5cd
Spencer <spencerartisan@gmail.com>
2025-03-03 19:39:29 +0000

    Separate name and id

--- src/app/thumbnail.js
+++ src/app/thumbnail.js
@@ -5,7 +5,7 @@ import styles from './thumbnail.module.scss';
 export default function UnderThumbnail({ item }) {
   return (
     <div>
-      <img src={`/images/${item.name}.jpg`} />
+      <img src={`/images/${item.id}.jpg`} />
       <div className={styles.addToBasket}>+</div>
     </div>
 
