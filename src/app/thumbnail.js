919919bf217d3ecaef3c49f2975f8237dec4fcc7
Spencer <spencerartisan@gmail.com>
2025-03-30 19:01:50 +0100

    Use webp files for images

--- src/app/thumbnail.js
+++ src/app/thumbnail.js
@@ -5,7 +5,7 @@ import styles from './thumbnail.module.scss';
 export default function Thumbnail({ item }) {
   return (
     <div>
-      <img className={styles.image} src={`/images/${item.id}.jpg`} />
+      <img className={styles.image} src={`/images/${item.id}.webp`} />
       <div className={styles.addToBasket}>+</div>
     </div>
 
