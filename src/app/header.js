adae3cdc151806e3a90db85bd63d1f2e81b290dc
Spencer <spencerartisan@gmail.com>
2025-04-11 10:01:55 +0100

    Restyle top bar

--- src/app/header.js
+++ src/app/header.js
@@ -4,7 +4,7 @@ import styles from './header.module.scss';
 
 export default function Header() {
   return (
-    <div id={styles.banner}>Free UK delivery on full price orders over £150. Free UK returns.</div>
+    <div id={styles.banner}>Free UK delivery on orders over £150. Free UK returns</div>
   );
 }
 
