3731f6d8e82eb65acfed9299385f856004ac3ad2
Spencer <spencerartisan@gmail.com>
2025-04-20 13:13:41 +0100

    Add blurb for retruns page

--- src/app/returns/page.js
+++ src/app/returns/page.js
@@ -1,7 +1,26 @@
+import styles from './page.module.scss';
+
 export default function ReturnsPage() {
   return (
-    <div>
-      ToDo
-    </div>
+    <main className={styles.main}>
+      <div className={styles.content}>
+        <h1>Returns & Refunds</h1>
+        <p>
+          At Marmalade, we want you to be completely satisfied with your purchase. 
+          We understand that sometimes items may need to be returned or exchanged. 
+        </p>
+        <p>
+        Returns are accepted within 30 days of purchase.
+        </p>
+
+          <h2>Questions?</h2>
+          <p>
+            If you have any questions about returns, please contact us at{' '}
+            <a href="mailto:returns@marmalade.com" className={styles.email}>
+              returns@marmalade.com
+            </a>
+          </p>
+      </div>
+    </main>
   );
 } 
