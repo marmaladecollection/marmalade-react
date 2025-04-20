3fab9f9008297e3ffe7f8cb31615e0cebf1f619d
Spencer <spencerartisan@gmail.com>
2025-04-20 13:06:48 +0100

    Remove Help from footer

--- src/app/footer.js
+++ src/app/footer.js
@@ -5,7 +5,6 @@ import styles from "./footer.module.scss";
 export default function Footer() {
   return (
     <div className={styles.footer}>
-      <a href="/help">Help</a>
       <a href="/delivery">Delivery</a>
       <a href="/returns">Returns</a>
       <a href="/contact">Contact Us</a>
