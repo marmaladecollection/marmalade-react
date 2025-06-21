79262018310890cafc3b4e0b119d4c2a93c914db
Spencer <spencerartisan@gmail.com>
2025-06-21 16:39:05 +0100

    Add Cypress test for undeliverable postcode error on checkout

---
+++ cypress/e2e/checkout.cy.js
@@ -0,0 +1,12 @@
+describe('Checkout', () => {
+  it('should show error for undeliverable postcode', () => {
+    cy.visit('/');
+    cy.contains('ADD').first().click();
+    cy.contains(/Go To Checkout|checkout|to checkout/i).click();
+    cy.get('input#line1').type('123 Test St');
+    cy.get('input#city').type('London');
+    cy.get('input#postcode').type('N5 2QT');
+    cy.contains('Continue to Payment').click();
+    cy.contains('Sorry, we do not deliver to this postcode.');
+  });
+}); 
