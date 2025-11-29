3f1ddba97adc4ecf44f7fd7a6e19f378fe3bad39
Spencer <spencerartisan@gmail.com>
2025-11-29 17:56:17 +0000

    Update About Us page content and adjust layout for better text flow

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -9,11 +9,11 @@ export default function AboutPage() {
         <div className={styles.textContainer}>
           <h1>About Us</h1>
           <p className={styles.text}>
-          Marmalade is a curated collection of antique furniture for sale. We offer a range of small pieces to fit any home. Each piece is hand-selected for its beauty and craftsmanship, as well as its practicality as a working item of furniture.
+          Marmalade is a curated collection of antique furniture for sale that offers a range of small pieces to fit any home. Each piece is hand-selected for its beauty and craftsmanship as well as its practicality as a working item of furniture.
           <br /><br />
-          Marmalade provides an accessible way to buy an antique desk, dining table or sofa at the same cost as purchasing new. We offer a personal alternative to vast online warehouses and overwhelming auctions.
+          At Marmalade, we provide an accessible way to buy an antique desk, dining table or sofa at the same cost as purchasing new. As a small, carefully assembled collection, we offer a distinctive and more personal alternative to online warehouses and overwhelming auctions.
           <br /><br />
-          Buying antique furniture is a great, sustainable choice! When you buy a piece of furniture that has already endured for a century or more, you know not only that it will last but also that you are preserving the past.
+          Our guiding belief at Marmalade is that buying antique furniture is a both practical and sustainable choice, and one that should not be daunting. When you buy a good piece of furniture that has already endured for a century or more, you know not only that it will last but also that you are preserving the past.
           </p>
         </div>
       </div>
